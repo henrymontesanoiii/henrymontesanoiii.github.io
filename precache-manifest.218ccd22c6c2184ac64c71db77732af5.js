81a73bc389d8c9049c02d609e50766310086920d
henman3 <henrymontesanoiii@gmail.com>
2018-12-04 16:23:02 -0500

    Updates

---
+++ precache-manifest.218ccd22c6c2184ac64c71db77732af5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "e94251455f3c16da5ee032497919e3dc",
+    "url": "/static/media/api.e9425145.png"
+  },
+  {
+    "revision": "3d9a33580e75fa804b22",
+    "url": "/static/css/main.8e6052ea.chunk.css"
+  },
+  {
+    "revision": "7c462abe59c1a5fa41e4",
+    "url": "/static/js/1.7c462abe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec0ef160916f6539cbaf162c87d93cbb",
+    "url": "/static/media/react.ec0ef160.jpg"
+  },
+  {
+    "revision": "db3850333408d11ab2355fa2c7c52ae7",
+    "url": "/static/media/bootstrap.db385033.png"
+  },
+  {
+    "revision": "4b5ff80043c881ad703eda8ae6c80f72",
+    "url": "/static/media/mongo.4b5ff800.jpg"
+  },
+  {
+    "revision": "c3c8f45bfc3864e6ac71f820479dc352",
+    "url": "/static/media/jquery.c3c8f45b.png"
+  },
+  {
+    "revision": "fcf95e722265486f54573a683bbfb6c5",
+    "url": "/static/media/mysql.fcf95e72.png"
+  },
+  {
+    "revision": "33e38ae9a3e264d3f21cc3cea96b5d42",
+    "url": "/static/media/js.33e38ae9.png"
+  },
+  {
+    "revision": "3d9a33580e75fa804b22",
+    "url": "/static/js/main.3d9a3358.chunk.js"
+  },
+  {
+    "revision": "444acf174e4507bc8151b0324874c97f",
+    "url": "/static/media/git.444acf17.jpeg"
+  },
+  {
+    "revision": "cf202eedeac0db2bc5d06a7e3b31b2f1",
+    "url": "/static/media/Widget.cf202eed.png"
+  },
+  {
+    "revision": "b6e7d63c32d97cf18afa0f7e051645eb",
+    "url": "/static/media/NEXT.b6e7d63c.png"
+  },
+  {
+    "revision": "3152ea6e30ab7c5d9b5469519ffd920d",
+    "url": "/static/media/scrap.3152ea6e.png"
+  },
+  {
+    "revision": "3a105b1af6b0346a528176223f74a130",
+    "url": "/static/media/Click.3a105b1a.png"
+  },
+  {
+    "revision": "91e175c856e7dda02c201ea170bbeea6",
+    "url": "/static/media/Trivia.91e175c8.png"
+  },
+  {
+    "revision": "3d396a11ecbb8272501820e0119bd6ba",
+    "url": "/static/media/d0f22ba21124dba8fe59dce25575ae67.3d396a11.gif"
+  },
+  {
+    "revision": "741f12b7e9b6dfcf29f8b3da10003e13",
+    "url": "/static/media/name.741f12b7.png"
+  },
+  {
+    "revision": "a74926d847160ff5c74bda18d3f22c54",
+    "url": "/static/media/MERN.a74926d8.png"
+  },
+  {
+    "revision": "e82dd7c2e883f9fce4a1d32e6b5ab382",
+    "url": "/index.html"
+  }
+];
